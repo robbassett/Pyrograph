810b09d6ad44947c6d74183533608f2b9969dee6
Robert Bassett <rbassett.astro@gmail.com>
2020-09-14 09:20:48 +1000

    no class

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from Pyrograph import *
+#from Pyrograph import *
 
 import dash
 import dash_core_components as dcc
@@ -204,6 +204,7 @@ def update_size_display(v1,v2):
     ]
 )
 def update_pyrograph(btn,fig,sz,ho,cl):
+    
     if btn == 0:
         fig = go.Figure(fig)
         fig.update_xaxes(range=[-1.,1.])
@@ -224,23 +225,11 @@ def update_pyrograph(btn,fig,sz,ho,cl):
         )
 
     
-    elif btn == 1:
-        fig = go.Figure(fig)
-        pyro = Pyrograph(hole=ho,inner_frac=sz)
-        for i in range(2): pyro.one_orbit()
-        fig.add_trace(go.Scatter(x=pyro.x[-1],y=pyro.y[-1],mode='lines',line=dict(color=cl['hex'])))
-        
-    else:
-        fig = go.Figure(fig)
-        pyro = Pyrograph(hole=ho,inner_frac=sz)
-        for i in range(btn+1): pyro.one_orbit()
-        fig.add_trace(go.Scatter(x=pyro.x[-1],y=pyro.y[-1],mode='lines',line=dict(color=cl['hex'])))
-    """
     else:
         fig = go.Figure(fig)
         x,y = one_orbit(sz,ho,btn)
         fig.add_trace(go.Scatter(x=x,y=y,mode='lines',line=dict(color=cl['hex'])))
-    """
+    
     return fig
 
 if __name__ == '__main__':
